156fde103e5907f233abb43670612acaa9563848
Naitik Lodha <naitik.lodha139@nmims.edu.in>
2024-11-22 17:42:27 +0530

    fixed bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "@/app/screens/hero";
 import About from "@/app/screens/about";
 import Pdomains from "@/app/screens/pdomains";
-import Sponsors from "@/app/screens/sponsors";
+// import Sponsors from "@/app/screens/sponsors";
 import FAQ from "@/app/screens/faq";
 import Contact from "@/app/screens/contact";
 

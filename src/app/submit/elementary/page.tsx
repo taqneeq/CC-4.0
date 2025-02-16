678f20d5b408586a9bd91745bc351cbcbb1931a5
Naitik Lodha <naitik.lodha139@nmims.edu.in>
2025-02-16 06:36:46 +0530

    add tally submission link

---
+++ src/app/submit/elementary/page.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="h-screen w-screen overflow-hidden">
+      <Head>
+        <title>Cyber Cypher 4.0 Elementary Submissions</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <script async src="https://tally.so/widgets/embed.js"></script>
+      </Head>
+      <iframe
+        src="https://tally.so/r/nGq2Wz"
+        className="absolute inset-0 w-full h-full border-0"
+        title="Cyber Cypher 4.0 Elementary Submissions"
+      ></iframe>
+    </div>
+  );
+}
